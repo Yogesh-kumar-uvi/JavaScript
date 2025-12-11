3a8c5cefee1cb97624a38e67194938f310830ef7
Yogesh kumar <yogeshkumar12112003@gmail.com>
2025-12-11 17:47:02 +0530

    51 lecture completed

---
+++ 51-innertext-vs-textcontent/app.js
@@ -0,0 +1,28 @@
+let box = document.getElementById("box");
+
+// console.log("textContent:", box.textContent);
+// console.log("innerText:", box.innerText);
+// console.log("innerHTML:", box.innerHTML);
+
+// // ------------------------------
+// // UPDATE HTML
+// // ------------------------------
+// box.innerHTML = `
+//     <h2>New Updated HTML</h2>
+//     <p>This HTML replaced the old one</p>
+// `;
+
+// // ------------------------------
+// // ADD NEW HTML
+// // ------------------------------
+// box.innerHTML += `<span style="color:blue;">Appended HTML content</span>`;
+
+// // ------------------------------
+// // UPDATE ONLY TEXT
+// // ------------------------------
+// box.textContent = "This is only text — no HTML";
+
+// // ------------------------------
+// // APPEND ONLY TEXT
+// // ------------------------------
+// box.textContent += " | more text added";
