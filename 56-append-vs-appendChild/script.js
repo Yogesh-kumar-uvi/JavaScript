95749850a9e22f3dc857db2d5924fc370958b01e
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-06 15:08:50 +0530

    56 lecture completed

--- 56-append-vs-appendChild/script.js
+++ 56-append-vs-appendChild/script.js
@@ -1,2 +1,19 @@
+const h1 = document.querySelector("h1")
 const container = document.querySelector(".container")
 const card = document.querySelector(".card")
+
+//container.appendChild(h1) only append
+//container.appendChild(h1.cloneNode(true)) append with content
+
+// for(let i = 2;i<=100;i++){
+
+// const newCard = card.cloneNode()
+// newCard.innerText = i
+// container.append(newCard)
+
+// }
+
+const newTextNode = document.createTextNode('Hello World')
+
+// container.appendChild(newTextNode)
+container.appendChild(newTextNode,h1)
