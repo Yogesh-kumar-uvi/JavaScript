43422077b80da815a6e3aa914dd4daa21c0ad8a9
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-13 13:40:52 +0530

    Form Events and Event Object completed

--- 60-form-event&event-object/script.js
+++ 60-form-event&event-object/script.js
@@ -1,6 +1,7 @@
 const usernameInput = document.querySelector('#username')
 const paragraph = document.querySelector('p')
 
+
 // usernameInput.addEventListener('click',() => {
 //     console.log("input clicked") 
 // })
@@ -9,8 +10,45 @@ const paragraph = document.querySelector('p')
 //     console.log("input dblclicked") 
 // })
 
-usernameInput.addEventListener('input',(e) => {
-    console.log(e.target.value)
-    paragraph.innerText = e.target.value
+// usernameInput.addEventListener('input',(e) => {
+//     console.log(e.target.value)
+//     inputValue = e.target.value
+//     paragraph.innerText = e.target.value
     
+// })
+
+
+// usernameInput.addEventListener('change',(e) => {
+//     console.log(e.target.value)
+//     inputValue = e.target.value
+//     paragraph.innerText = e.target.value
+    
+// })
+
+// usernameInput.addEventListener('focus',(e) => {
+// console.log(e.type)
+//     // console.log(e.target.value)
+//     inputValue = e.target.value
+//     paragraph.innerText = e.target.value
+// })
+
+// usernameInput.addEventListener('blur',(e) => {
+//     console.log(e.type)
+//     // console.log(e.target.value)
+//     inputValue = e.target.value
+//     paragraph.innerText = e.target.value
+    
+// })
+
+const form = document.querySelector('form')
+
+// form.addEventListener('submit',(e) => {
+//     e.preventDefault()
+// console.log("form Submited")
+// })
+
+form.addEventListener('click',(e) => {
+    e.preventDefault()
+console.log(e.target)
+console.log(e.currentTarget)
 })
