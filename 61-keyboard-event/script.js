866fad7a8ac3d9b55615cd78d5e9b0e1245c2aa4
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-15 18:57:22 +0530

    Keyboard Events in Javascript completed

---
+++ 61-keyboard-event/script.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+
+// h1.addEventListener('keypress',(e)=>{
+//     console.log('value:' , e.key);
+//     console.log('code:' , e.code);
+// })
+
+// h1.addEventListener('keydown',(e)=>{
+//     console.log('value:' , e.key);
+//     console.log('code:' , e.code);
+// })
+
+// h1.addEventListener('keyup',(e)=>{
+//     console.log('value:' , e.key);
+//     console.log('code:' , e.code);
+// })
