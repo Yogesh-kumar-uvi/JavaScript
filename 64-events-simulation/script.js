456a0fd5efff853a7df4ae8587b73ddf1579b1a3
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-18 14:46:31 +0530

    Event Simulators

--- 64-events-simulation/script.js
+++ 64-events-simulation/script.js
@@ -1,5 +1,7 @@
 const addCardBtn = document.querySelector('.card')
 const container = document.querySelector('.container')
+const input = document.querySelector('input')
+const form =  document.querySelector('form')
 
 let count = 1
 
@@ -9,3 +11,28 @@ addCardBtn.addEventListener('click', () => {
     newCard.innerText = count++
     container.append(newCard)
 })
+
+// setInterval(()=>{
+//     console.log("form submited")
+//     form.submit()
+// },3000)
+
+// setInterval(()=>{
+//     input.focus()
+//     console.log("input focused")
+// },1000)
+
+// setInterval(()=>{
+//     input.blur()
+//     console.log("input blured")
+// },5000)
+
+
+//  const intervalId = setInterval( () => {
+//     console.log(intervalId)
+//     if(count >= 1000){
+//         clearInterval(intervalId)
+//     }
+//     addCardBtn.click()
+// },10)
+
