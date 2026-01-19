90e00c47aa281c128685a6998aa6589d85abdd33
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-19 13:28:30 +0530

    Event Deligation

---
+++ 65-event-delegation/script.js
@@ -0,0 +1,18 @@
+const addCardBtn = document.querySelector('.add-card')
+const container = document.querySelector('.container')
+
+let count = 1;
+
+addCardBtn.addEventListener('click', () => {
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+
+container.addEventListener('click',(e)=>{
+    if(e.target !== container){
+        e.target.remove()
+    }
+})
+
