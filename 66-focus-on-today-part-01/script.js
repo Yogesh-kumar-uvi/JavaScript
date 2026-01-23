82332a6cad059683dfe3fee889176e30e9ec7be6
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-01-23 21:49:17 +0530

    local storage used in project

--- 66-focus-on-today-part-01/script.js
+++ 66-focus-on-today-part-01/script.js
@@ -4,6 +4,8 @@ const errorLebel = document.querySelector('.error-label')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 
+const allGoals = JSON.parse(localStorage.getItem('allGoals'))|| {}
+
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener('click', (e) => {
         const allGoalsAdded = [...inputFields].every(function (input) {
@@ -13,14 +15,29 @@ checkBoxList.forEach((checkbox) => {
         if (allGoalsAdded) {
             checkbox.parentElement.classList.toggle('completed')
             progressValue.style.width = '33.33%'
+            const inputId = checkbox.nextElementSibling.id
+            allGoals[inputId].completed =  !allGoals[inputId].completed
+            localStorage.setItem('allGoals',JSON.stringify(allGoals))
         } 
         else {
             progressBar.classList.add('show-error')
         }
     })
 })
+
 inputFields.forEach((input) => {
+    input.value = allGoals[input.id].name
+    if(allGoals[input.id].completed) {
+        input.parentElement.classList.add('completed')
+    }
     input.addEventListener('focus', () => {
         progressBar.classList.remove('show-error')
+    })
+    input.addEventListener('input',(e) => {
+        allGoals[input.id] = {
+            name:input.value,
+            completed:false
+        }
+        localStorage.setItem('allGoals',JSON.stringify(allGoals))
     })
 })
