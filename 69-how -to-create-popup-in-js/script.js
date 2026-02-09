b97fc228d9c8bbd2dd66e3c5ee49ae35e9031922
Yogesh kumar <yogeshkumar12112003@gmail.com>
2026-02-09 23:38:27 +0530

    by 1st method this popup completed

--- 69-how -to-create-popup-in-js/script.js
+++ 69-how -to-create-popup-in-js/script.js
@@ -1,10 +1,14 @@
 const button = document.querySelector('button')
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup-container')
 const closeIcon = document.querySelector('.close-icon')
+const overlay =  document.querySelector('.overlay')
 
 button.addEventListener('click',()=>{
-    popup.classList.add('open')
+    popup.classList.add('popup-open')
 })
 closeIcon.addEventListener('click',()=>{
-    popup.classList.remove('open')
+    popup.classList.remove('popup-open')
+})
+overlay.addEventListener('click',()=>{
+    popup.classList.remove('popup-open')
 })
